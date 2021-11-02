f4223af9563bcb6d6d7de385f5ced0b516f567d8
Mohd Amir <contactus@negbuy.com>
2021-11-02 14:31:53 +0530

    api update

--- negbuyapi/urls.py
+++ negbuyapi/urls.py
@@ -8,6 +8,8 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     #path('', views.homepageView, name="homepage"),
     path('api/product_list', views.product_list, name="product_list"),
+    path('api/mostvalues_fastdispatch', views.mostvalued_fastdispatch, name="mostvalued_fastdispatch"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- negbuyapi/views.py
+++ negbuyapi/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
+import random
 
 # Create your views here.
 
@@ -29,6 +30,57 @@ def product_list(request):
         product_list_.append(each_obj)
 
     return Response(product_list_, status=200)
+
+
+@api_view(['GET'])
+def mostvalued_fastdispatch(request):
+
+    #in future will be changed according to user 
+    #for now testing version
+    all_products = list(Product_db.objects.all())
+
+    Most_valued_product = random.sample(all_products, 4)
+    most_valued = list()
+    fast_dispatch = list()
+
+    for i in Most_valued_product:
+        qwe = {
+            'id':i.id,
+            'title':i.title,
+            'price':i.price,
+            'description':i.description,
+            'category':None,
+            'image':i.image.url,
+            'rating':{
+                'rate':3.0,
+                'count':430
+            }
+        }
+
+        most_valued.append(qwe)
+    
+    for i in all_products:
+        if len(fast_dispatch) < 4:
+            if i.fast_dispatch == 'True':
+                qwe = {
+                    'id':i.id,
+                    'title':i.title,
+                    'price':i.price,
+                    'description':i.description,
+                    'category':None,
+                    'image':i.image.url,
+                    'rating':{
+                        'rate':3.0,
+                        'count':430
+                    }
+                }
+
+                fast_dispatch.append(qwe)
+        else:
+            break
+
+
+    return Response({'Most_valued':most_valued, 'fast_dispatch':fast_dispatch}, status=200)
 
 
 
